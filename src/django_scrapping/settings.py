ed13ff0d6d3a7b48db781979d30c359c893f80cc
ilnurav <ilnurakhmetov@gmail.com>
2021-03-21 18:06:54 +0500

    update secret keys

--- src/django_scrapping/settings.py
+++ src/django_scrapping/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secretkeys import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0-mb7nbjgt7vl@rs%&7sml@%==09g^z22rt%d*v!%kv1^1*l*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
