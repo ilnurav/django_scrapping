e0fff55e348d44cb6a36a73b194e4570fee0b5f3
ilnurav <ilnurakhmetov@gmail.com>
2021-04-04 16:02:18 +0500

    class Language

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,5 +1,40 @@
 from django.db import models
+from scraping. utils import from_cyrillic_to_eng
 
 class City(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50,
+                            verbose_name='Название населенного пункта',
+                            unique=True)
+    slug = models.CharField(max_length=50,
+                            blank=True)
+
+    class Meta:
+        verbose_name = 'Название населенного пункта'
+        verbose_name_plural = 'Название населенных пунктов'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Язык программирования',
+                            unique=True)
+    slug = models.CharField(max_length=50,
+                            blank=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
